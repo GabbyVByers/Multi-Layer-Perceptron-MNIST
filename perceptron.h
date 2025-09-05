8181e52d8b21141a64dfc5c0a90cffaf17cead55
Gabby <GabbyVByers@gmail.com>
2025-09-04 20:05:28 -0700

    much better

--- perceptron.h
+++ perceptron.h
@@ -1,81 +1,48 @@
 #pragma once
 
+#include <vector>
+
 class Perceptron
 {
 public:
 
-	float**  layers = nullptr;
-	float**  biases = nullptr;
+	float**  layers  = nullptr;
+	float**  biases  = nullptr;
 	float*** weights = nullptr;
+
+	int numLayers = -1;
+	const std::vector<int> networkStructure = { 784, 16, 16, 10 };
 
 	Perceptron()
 	{
-		layers = new float* [4];
-		biases = new float* [3];
-		weights = new float** [3];
+		numLayers = networkStructure.size();
 
-		layers[0] = new float[784];
-		layers[1] = new float[16];
-		layers[2] = new float[16];
-		layers[3] = new float[10];
-
-		biases[0] = new float[16];
-		biases[1] = new float[16];
-		biases[2] = new float[10];
-
-		weights[0] = new float* [784];
-		weights[1] = new float* [16];
-		weights[2] = new float* [16];
-
-		for (int i = 0; i < 784; i++)
+		layers = new float* [numLayers];
+		for (int i = 0; i < numLayers; i++)
 		{
-			weights[0][i] = new float[16];
+			layers[i] = new float[networkStructure[i]];
 		}
 
-		for (int i = 0; i < 16; i++)
+		biases = new float* [numLayers - 1];
+		for (int i = 0; i < numLayers - 1; i++)
 		{
-			weights[1][i] = new float[16];
+			biases[i] = new float[networkStructure[i + 1]];
 		}
 
-		for (int i = 0; i < 16; i++)
+		weights = new float** [numLayers - 1];
+		for (int i = 0; i < numLayers - 1; i++)
 		{
-			weights[2][i] = new float[10];
+			weights[i] = new float* [networkStructure[i]];
+			for (int j = 0; j < networkStructure[i]; i++)
+			{
+				weights[i][j] = new float[networkStructure[i + 1]];
+			}
 		}
 	}
 
 	~Perceptron()
 	{
-		for (int i = 0; i < 784; i++)
-		{
-			delete[] weights[0][i];
-		}
-
-		for (int i = 0; i < 16; i++)
-		{
-			delete[] weights[1][i];
-		}
-
-		for (int i = 0; i < 16; i++)
-		{
-			delete[] weights[2][i];
-		}
-
-		delete[] weights[0];
-		delete[] weights[1];
-		delete[] weights[2];
-
-		delete[] biases[0];
-		delete[] biases[1];
-		delete[] biases[2];
-
-		delete[] layers[0];
-		delete[] layers[1];
-		delete[] layers[2];
-		delete[] layers[3];
-
-		delete[] layers;
-		delete[] biases;
-		delete[] weights;
+		
 	}
 };
 
